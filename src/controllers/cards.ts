b2a6f0d030500979923cc9295f27bfcb7841cd2b
SmykNikolay <157750278+SmykNikolay@users.noreply.github.com>
2024-03-26 15:55:21 +0300

    init

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,21 +1,86 @@
+import { Request, Response } from 'express';
+import { STATUS_CODES, ERROR_MESSAGES } from '../utils/errors';
 import Card from '../model/card';
+import { MyRequest } from '../utils/types';
 
-export async function getAllCards() {
-  return Card.find();
+export async function getAllCards(_req:Request, res:Response) {
+  try {
+    const cards = await Card.find();
+    res.send(cards);
+  } catch (err) {
+    res.status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+  }
+}
+export async function createCard(req:Request, res:Response) {
+  try {
+    const card = await Card.create(req.body);
+    res.status(STATUS_CODES.CREATED).send(card);
+  } catch (err) {
+    res.status(STATUS_CODES.BAD_REQUEST).send({ message: ERROR_MESSAGES.INVALID_CARD_DATA });
+  }
 }
 
-export async function createCard(data: { name: string; link: string; }) {
-  return Card.create(data);
+export async function getCard(req:Request, res:Response) {
+  try {
+    const card = await Card.findById(req.params.cardId);
+    if (!card) {
+      return res.status(STATUS_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.CARD_NOT_FOUND });
+    }
+    return res.send(card);
+  } catch (err) {
+    return res.status(STATUS_CODES.BAD_REQUEST).send({ message: ERROR_MESSAGES.INVALID_ID });
+  }
 }
 
-export async function deleteCard(cardId: string) {
-  return Card.findByIdAndDelete(cardId);
+export async function deleteCard(req:Request, res:Response) {
+  try {
+    const card = await Card.findByIdAndDelete(req.params.cardId);
+    if (!card) {
+      return res.status(STATUS_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.CARD_NOT_FOUND });
+    }
+    return res.send(card);
+  } catch (err) {
+    return res.status(STATUS_CODES.BAD_REQUEST).send({ message: ERROR_MESSAGES.INVALID_ID });
+  }
 }
 
-export async function likeCard(cardId: string, userId: string) {
-  return Card.findByIdAndUpdate(cardId, { $addToSet: { likes: userId } }, { new: true });
+export async function likeCard(req:MyRequest, res:Response) {
+  try {
+    if (req.user === undefined || req.user._id === undefined) {
+      return res.status(STATUS_CODES.UNAUTHORIZED)
+        .send({ message: ERROR_MESSAGES.USER_NOT_AUTHORIZED });
+    }
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    );
+    if (!card) {
+      return res.status(STATUS_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.INVALID_CARD_ID });
+    }
+    return res.send(card);
+  } catch (err) {
+    return res.status(STATUS_CODES.BAD_REQUEST).send({ message: ERROR_MESSAGES.INVALID_LIKE_DATA });
+  }
 }
 
-export async function dislikeCard(cardId: string, userId: string) {
-  return Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true });
+export async function dislikeCard(req:MyRequest, res:Response) {
+  try {
+    if (req.user === undefined || req.user._id === undefined) {
+      return res.status(STATUS_CODES.UNAUTHORIZED)
+        .send({ message: ERROR_MESSAGES.USER_NOT_AUTHORIZED });
+    }
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true },
+    );
+    if (!card) {
+      return res.status(STATUS_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.INVALID_CARD_ID });
+    }
+    return res.send(card);
+  } catch (err) {
+    return res.status(STATUS_CODES.BAD_REQUEST).send({ message: ERROR_MESSAGES.INVALID_LIKE_DATA });
+  }
 }
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,23 +1,90 @@
+import { Request, Response } from 'express';
+import { MyRequest } from '../utils/types';
 import User from '../model/user';
+import { STATUS_CODES, ERROR_MESSAGES } from '../utils/errors';
 
-export async function getAllUsers() {
-  return User.find({});
+export async function getAllUsers(_req : Request, res:Response) {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({ message: 'Server error' });
+  }
 }
 
-export async function getUserById(id: string) {
-  return User.findById(id);
+export async function getUserById(req : Request, res:Response) {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(STATUS_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.USER_NOT_FOUND });
+    }
+    return res.send(user);
+  } catch (err) {
+    if ((err as Error).name === 'CastError') {
+      return res.status(STATUS_CODES.BAD_REQUEST).send({ message: ERROR_MESSAGES.INVALID_USER_ID });
+    }
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+  }
+}
+export async function createUser(req : Request, res:Response) {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.status(STATUS_CODES.CREATED).send(user);
+  } catch (err) {
+    if ((err as Error).name === 'ValidationError') {
+      return res.status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: ERROR_MESSAGES.INVALID_USER_DATA });
+    }
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+  }
 }
 
-export async function createUser(data: { name: string; about: string; avatar: string }) {
-  const user = new User(data);
-  await user.save();
-  return user;
+export async function updateUserProfile(req : MyRequest, res:Response) {
+  if (req.user === undefined || req.user._id === undefined) {
+    return res.status(STATUS_CODES.UNAUTHORIZED)
+      .send({ message: ERROR_MESSAGES.USER_NOT_AUTHORIZED });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, req.body, { new: true });
+    if (!user) {
+      return res.status(STATUS_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.NOT_FOUND });
+    }
+    return res.send(user);
+  } catch (err) {
+    if ((err as Error).name === 'ValidationError') {
+      return res.status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: ERROR_MESSAGES.INVALID_USER_DATA_UPDATE });
+    }
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+  }
 }
-
-export async function updateUserProfile(userId: string, data: { name: string; about: string; }) {
-  return User.findByIdAndUpdate(userId, data, { new: true });
-}
-
-export async function updateUserAvatar(userId: string, avatar: string) {
-  return User.findByIdAndUpdate(userId, { avatar }, { new: true });
+export async function updateUserAvatar(req : MyRequest, res:Response) {
+  if (req.user === undefined || req.user._id === undefined) {
+    return res.status(STATUS_CODES.UNAUTHORIZED)
+      .send({ message: ERROR_MESSAGES.USER_NOT_AUTHORIZED });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar: req.body.avatar },
+      { new: true },
+    );
+    if (!user) {
+      return res.status(STATUS_CODES.NOT_FOUND)
+        .send({ message: ERROR_MESSAGES.NOT_FOUND });
+    }
+    return res
+      .send(user);
+  } catch (err) {
+    if ((err as Error).name === 'ValidationError') {
+      return res.status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: ERROR_MESSAGES.INVALID_AVATAR });
+    }
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+  }
 }
